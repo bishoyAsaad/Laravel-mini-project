9d8407eb850836b66cd1c64184bf5f5b7de99613
bishoy <bishoy.g.asaad@gmail.com>
2019-12-01 17:30:37 +0200

    Display Author next to user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,10 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name}} @if(Auth::user()->role==1)<b>Admin</b>@endif<span class="caret"></span>
+                                    {{ Auth::user()->name}} 
+                                    @if(Auth::user()->role==1)<b>Admin</b>@endif
+                                    @if(Auth::user()->role==2)<b>Author</b>@endif
+                                    <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
